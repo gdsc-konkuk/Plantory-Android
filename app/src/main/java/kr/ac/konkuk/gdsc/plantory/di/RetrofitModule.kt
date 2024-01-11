3d138f3007186141006fb9704aecd2f442eb55ec
HyeseonBaek <konkukbhs@gmail.com>
2024-01-11 15:51:15 +0900

    [add/#1] RetrofitModule 생성

---
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/di/RetrofitModule.kt
@@ -0,0 +1,69 @@
+package kr.ac.konkuk.gdsc.plantory.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import kr.ac.konkuk.gdsc.plantory.di.qualifier.Auth
+import kr.ac.konkuk.gdsc.plantory.di.qualifier.Logger
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    private const val CONTENT_TYPE = "application/json"
+
+    @Provides
+    @Singleton
+    fun provideJsonConverter(json: Json): Converter.Factory =
+        json.asConverterFactory(CONTENT_TYPE.toMediaType())
+
+    @Provides
+    @Singleton
+    @Logger
+    fun provideHttpLoggingInterceptor(): Interceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        @Logger loggingInterceptor: Interceptor,
+        @Auth interceptor: Interceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .addInterceptor(interceptor)
+        .build()
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient,
+        factory: Converter.Factory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl("BASE_URL_HERE")
+        .client(client)
+        .addConverterFactory(factory)
+        .build()
+
+
+}
+
+
