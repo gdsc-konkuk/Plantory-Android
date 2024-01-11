ccd63437ff10b37bebfc6d252396f73c7c5910a6
HyeseonBaek <konkukbhs@gmail.com>
2024-01-11 15:08:05 +0900

    [add/#1] Version Catalog 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,53 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Kotlin
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
+
+    // Retrofit and OkHttp
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+
+    // Jetpack
+    implementation(libs.hilt.android)
+    implementation(libs.room.runtime)
+
+    // Third-party
+    implementation(libs.timber)
+    implementation(libs.coil)
+    implementation(libs.lottie)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.remoteConfig)
+    implementation(libs.firebase.database)
+
+    // Other AndroidX libraries
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.splashscreen)
+
+    // Navigation
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+
+    // Coil
+    implementation(libs.coil)
+
+    // Lottie
+    implementation(libs.lottie)
+
+    // DataStore
+    implementation(libs.androidx.datastore.core)
+    implementation(libs.androidx.datastore.preferences)
 }
