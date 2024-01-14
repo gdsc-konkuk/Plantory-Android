8ff44592af87799f94a766dc6b755af413ef20a1
HyeseonBaek <konkukbhs@gmail.com>
2024-01-15 03:21:57 +0900

    [feat/#5] 홈 식물 viewpager adapter 구현

---
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/home/HomeAdapter.kt
@@ -0,0 +1,35 @@
+package kr.ac.konkuk.gdsc.plantory.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.konkuk.gdsc.plantory.databinding.ItemHomePlantBinding
+import kr.ac.konkuk.gdsc.plantory.domain.entity.Plant
+import kr.ac.konkuk.gdsc.plantory.util.view.ItemDiffCallback
+
+class HomeAdapter : androidx.recyclerview.widget.ListAdapter<Plant, HomeAdapter.HomeViewHolder>(
+    ItemDiffCallback<Plant>(
+        onItemsTheSame = { old, new -> old.id == new.id },
+        onContentsTheSame = { old, new -> old == new }
+    )
+) {
+
+    class HomeViewHolder(
+        private val binding: ItemHomePlantBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: Plant) {
+            binding.data = data
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val binding =
+            ItemHomePlantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+}
