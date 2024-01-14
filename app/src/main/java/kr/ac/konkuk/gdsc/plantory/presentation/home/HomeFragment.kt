32dbe93d42aaab610baaf2fc0c870771a8bf0b9e
HyeseonBaek <konkukbhs@gmail.com>
2024-01-15 03:23:32 +0900

    [feat/#5] 홈 viewpager, indicator 구현

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/home/HomeFragment.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/home/HomeFragment.kt
@@ -1,10 +1,46 @@
 package kr.ac.konkuk.gdsc.plantory.presentation.home
 
+import android.os.Bundle
+import android.view.View
 import dagger.hilt.android.AndroidEntryPoint
 import kr.ac.konkuk.gdsc.plantory.R
 import kr.ac.konkuk.gdsc.plantory.databinding.FragmentHomeBinding
+import kr.ac.konkuk.gdsc.plantory.domain.entity.Plant
 import kr.ac.konkuk.gdsc.plantory.util.binding.BindingFragment
 
 @AndroidEntryPoint
 class HomeFragment  : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home){
+    private lateinit var homeAdapter: HomeAdapter
+    private lateinit var viewModel: HomeViewModel
+    private var plantItemCount = 0
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel = HomeViewModel()
+        initMockData()
+    }
+
+    private fun initMockData(){
+        initPlantViewPager(viewModel.plantList)
+    }
+    private fun initPlantViewPager(plants: List<Plant>) {
+        initPlantViewPagerAdapter(plants = plants)
+        initPlantViewPagerIndicator(plants = plants)
+    }
+    private fun initPlantViewPagerAdapter(plants: List<Plant>) {
+        homeAdapter = HomeAdapter().apply {
+            binding.vpHomePlant.adapter = this
+            submitList(plants)
+        }
+    }
+
+
+    private fun initPlantViewPagerIndicator(plants: List<Plant>) {
+        binding.diHomePlant.apply {
+            attachTo(binding.vpHomePlant)
+            plantItemCount = plants.size
+            addDot(plantItemCount)
+        }
+    }
+
 }
