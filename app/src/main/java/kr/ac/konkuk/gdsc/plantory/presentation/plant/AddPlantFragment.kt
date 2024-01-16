18dd7f61bc58b0a2826372c510ebcf2d59d23ca2
suyeun84 <kelsey6225@naver.com>
2024-01-16 15:39:49 +0900

    [feat/#6] 식물종 입력자동완성 구현

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/plant/AddPlantFragment.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/plant/AddPlantFragment.kt
@@ -3,8 +3,12 @@ package kr.ac.konkuk.gdsc.plantory.presentation.plant
 import android.app.DatePickerDialog
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.adapters.TextViewBindingAdapter.setText
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,15 +18,21 @@ import kr.ac.konkuk.gdsc.plantory.util.binding.BindingFragment
 import java.util.Calendar
 
 @AndroidEntryPoint
-class AddPlantFragment: BindingFragment<FragmentAddPlantBinding>(R.layout.fragment_add_plant) {
+class AddPlantFragment : BindingFragment<FragmentAddPlantBinding>(R.layout.fragment_add_plant) {
 
     private var selectedImageUri: Uri? = null
+    private lateinit var viewModel: AddPlantViewModel
+    private lateinit var adapter: ArrayAdapter<String>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel = AddPlantViewModel()
         openGallery()
         initDatePicker()
+        initPlantSpeciesWordNum()
+        autoCompletePlantSpecies()
+        initRegisterBtn()
     }
 
     private val getContent =
@@ -61,4 +71,33 @@ class AddPlantFragment: BindingFragment<FragmentAddPlantBinding>(R.layout.fragme
         }
     }
 
+    private fun initPlantSpeciesWordNum() {
+        binding.tvAddplantSpecies.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val currLength = binding.tvAddplantSpecies.length()
+                binding.counterText.text = "$currLength/14"
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun autoCompletePlantSpecies() {
+        adapter =
+            ArrayAdapter<String>(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                viewModel.plantSpeciesList
+            )
+
+        binding.tvAddplantSpecies.setAdapter(adapter)
+
+        binding.tvAddplantSpecies.setOnItemClickListener { parent, view, position, id ->
+            val item = parent.getItemAtPosition(position).toString()
+            binding.tvAddplantSpecies.setText(item)
+        }
+    }
 }
