45bb88b74f0502677eab60007c6d896e69e86410
HyeseonBaek <konkukbhs@gmail.com>
2024-01-17 13:38:04 +0900

    [feat/#12] 이미지 url로 불러오는 bindingAdapter 구현

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/util/binding/BindingAdapter.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/util/binding/BindingAdapter.kt
@@ -1,1 +1,15 @@
 package kr.ac.konkuk.gdsc.plantory.util.binding
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+import coil.transform.RoundedCornersTransformation
+
+@BindingAdapter("setImageUrl")
+fun ImageView.setImageUrl(imageUrl: String?) {
+    if (imageUrl == null) return
+    load(imageUrl) {
+        transformations(RoundedCornersTransformation(10F))
+    }
+}
+
