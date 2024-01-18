2675728954bf1a10da99b5270cede7113c5d8a37
suyeun84 <kelsey6225@naver.com>
2024-01-18 17:44:20 +0900

    [add/#11] BindingAdapter 추가

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/util/binding/BindingAdapter.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/util/binding/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package kr.ac.konkuk.gdsc.plantory.util.binding
 
+import android.widget.Button
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
@@ -17,4 +18,9 @@ fun ImageView.setImageUrl(imageUrl: String?) {
 @BindingAdapter("setCheckBoxImage")
 fun ImageView.setCheckBoxImage(isChecked: Boolean) {
     load(if (isChecked) R.drawable.ic_diary_checked else R.drawable.ic_diary_unchecked)
+}
+
+@BindingAdapter("setRegisterBackgroundResource")
+fun Button.setRegisterBackgroundResource(isFieldNotEmpty: Boolean) {
+    setBackgroundResource(if (isFieldNotEmpty) R.drawable.shape_mint_fill_10 else R.drawable.shape_gray_200_fill_10)
 }
