6d136edaefa3e1d777ca05a06b7acb7eadd67c39
HyeseonBaek <konkukbhs@gmail.com>
2024-01-20 16:51:30 +0900

    [feat/#17] 현재 토큰 확인

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/MainActivity.kt
@@ -1,9 +1,12 @@
 package kr.ac.konkuk.gdsc.plantory.presentation
 
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import kr.ac.konkuk.gdsc.plantory.R
 import kr.ac.konkuk.gdsc.plantory.databinding.ActivityMainBinding
@@ -12,8 +15,17 @@ import kr.ac.konkuk.gdsc.plantory.util.binding.BindingActivity
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel.getDeviceToken()
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Log.d("Current token", "Current token: $token")
+        }
         navigateTo<HomeFragment>()
     }
 
