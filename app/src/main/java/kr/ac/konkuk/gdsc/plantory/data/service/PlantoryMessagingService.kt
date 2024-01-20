c3e9719233811ab019dcefb658e0967015e12a66
HyeseonBaek <konkukbhs@gmail.com>
2024-01-20 16:49:58 +0900

    [feat/#17] Firebase MessagingService

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/data/service/PlantoryMessagingService.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/data/service/PlantoryMessagingService.kt
@@ -1,11 +1,74 @@
 package kr.ac.konkuk.gdsc.plantory.data.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.ac.konkuk.gdsc.plantory.R
+import kr.ac.konkuk.gdsc.plantory.domain.repository.DataStoreRepository
+import kr.ac.konkuk.gdsc.plantory.presentation.MainActivity
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class PlantoryMessagingService : FirebaseMessagingService(){
+class PlantoryMessagingService : FirebaseMessagingService() {
+    @Inject
+    lateinit var dataStoreRepository: DataStoreRepository
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStoreRepository.saveDeviceToken(token)
+        }
     }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        if (remoteMessage.notification != null) {
+            sendNotification(remoteMessage)
+        } else {
+            Log.e("TAG", "Notification Empty")
+        }
+    }
+
+
+    private fun sendNotification(remoteMessage: RemoteMessage) {
+        val id = 0
+        val title = remoteMessage.notification!!.title
+        val body = remoteMessage.notification!!.body
+
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, id, intent,
+            (System.currentTimeMillis() / 7).toInt()
+        )
+
+        val channelId = "Channel ID"
+        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+            .setSound(soundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(channelId, "Notice", NotificationManager.IMPORTANCE_HIGH)
+
+        notificationManager.createNotificationChannel(channel)
+        notificationManager.notify(id, notificationBuilder.build())
+    }
+
 }
