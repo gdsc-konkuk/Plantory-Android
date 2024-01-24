024c45dbed95b8e501c67a9d4e4800bfadbb6c14
HyeseonBaek <konkukbhs@gmail.com>
2024-01-24 17:17:00 +0900

    [feat/#21] 서버 연결 제외한 뼈대 구현

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/plant/diary/DiaryFragment.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/plant/diary/DiaryFragment.kt
@@ -1,12 +1,20 @@
 package kr.ac.konkuk.gdsc.plantory.presentation.plant.diary
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kr.ac.konkuk.gdsc.plantory.R
 import kr.ac.konkuk.gdsc.plantory.databinding.FragmentDiaryBinding
 import kr.ac.konkuk.gdsc.plantory.util.binding.BindingFragment
+import kr.ac.konkuk.gdsc.plantory.util.fragment.snackBar
+import kr.ac.konkuk.gdsc.plantory.util.fragment.viewLifeCycle
+import kr.ac.konkuk.gdsc.plantory.util.fragment.viewLifeCycleScope
+import kr.ac.konkuk.gdsc.plantory.util.view.UiState
 
 @AndroidEntryPoint
 class DiaryFragment : BindingFragment<FragmentDiaryBinding>(R.layout.fragment_diary) {
@@ -14,10 +22,27 @@ class DiaryFragment : BindingFragment<FragmentDiaryBinding>(R.layout.fragment_di
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initMockData()
+        setGetPantDailyRecordStateObserver()
     }
 
-    private fun initMockData() {
-        binding.data = viewModel.plantRecord
+    private fun setGetPantDailyRecordStateObserver() {
+        viewModel.getPlantDailyRecordState.flowWithLifecycle(viewLifeCycle).onEach { state ->
+            when (state) {
+                is UiState.Success -> {
+                    binding.data = state.data
+                }
+
+                is UiState.Failure -> {
+                    snackBar(binding.root) { state.msg }
+                }
+
+                is UiState.Empty -> {
+                }
+
+                is UiState.Loading -> {
+                }
+            }
+
+        }.launchIn(viewLifeCycleScope)
     }
 }
--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/plant/diary/DiaryViewModel.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/plant/diary/DiaryViewModel.kt
@@ -1,23 +1,47 @@
 package kr.ac.konkuk.gdsc.plantory.presentation.plant.diary
 
 import androidx.lifecycle.ViewModel
-import kr.ac.konkuk.gdsc.plantory.domain.entity.PlantCheckRecord
-import kr.ac.konkuk.gdsc.plantory.domain.entity.PlantDailyRecord
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kr.ac.konkuk.gdsc.plantory.data.dto.response.ResponseGetPlantDailyRecord
+import kr.ac.konkuk.gdsc.plantory.domain.repository.PlantRepository
+import kr.ac.konkuk.gdsc.plantory.util.view.UiState
+import retrofit2.HttpException
+import timber.log.Timber
+import javax.inject.Inject
 
-class DiaryViewModel : ViewModel() {
-    val plantRecord: PlantDailyRecord = generateMockData()
+@HiltViewModel
+class DiaryViewModel @Inject constructor(
+    private val plantRepository: PlantRepository
+) : ViewModel() {
 
-    private fun generateMockData(): PlantDailyRecord {
-        return PlantDailyRecord(
-            id = 1,
-            imageUrl = "https://plchldr.co/i/400x700?&bg=D4E1E4&fc=46AEA1&text=hihiplantory",
-            date = "2024/01/17",
-            nickname = "초록이",
-            comment = "자고 일어나니까 그새 좀 푸릇해진 것 같은 느낌이 들어서 몹시 뿌듯했당.... 우리초록이너무귀여워사랑해너는세상에서가장멋진다육이야 기죽지마어깨펴니가짱이야",
-            checkRecord = PlantCheckRecord(
-                isRepoted = true,
-                isWatered = false
-            )
-        )
+    private val _getPlantDailyRecordState =
+        MutableStateFlow<UiState<ResponseGetPlantDailyRecord>>(UiState.Loading)
+    val getPlantDailyRecordState: StateFlow<UiState<ResponseGetPlantDailyRecord>> =
+        _getPlantDailyRecordState.asStateFlow()
+
+    init {
+        getPlantDailyRecord()
+    }
+
+    private fun getPlantDailyRecord() {
+        viewModelScope.launch {
+            plantRepository.getPlantDailyRecord()
+                .onSuccess { response ->
+                    _getPlantDailyRecordState.value = UiState.Success(response)
+                    Timber.e("성공 $response")
+                }
+                .onFailure { t ->
+                    if (t is HttpException) {
+                        Timber.e("HTTP 실패")
+                    }
+                    Timber.e("${t.message}")
+                    _getPlantDailyRecordState.value = UiState.Failure("${t.message}")
+                }
+        }
     }
 }
