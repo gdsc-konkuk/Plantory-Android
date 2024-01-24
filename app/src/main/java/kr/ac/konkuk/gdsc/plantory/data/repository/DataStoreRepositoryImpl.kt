dc311e0ed452a64b0eb636335158500c656f4500
HyeseonBaek <konkukbhs@gmail.com>
2024-01-24 14:33:26 +0900

    [mod/#17] deviceToken Nullable 하게 구현체 통일

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/data/repository/DataStoreRepositoryImpl.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/data/repository/DataStoreRepositoryImpl.kt
@@ -5,6 +5,8 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kr.ac.konkuk.gdsc.plantory.domain.repository.DataStoreRepository
 import javax.inject.Inject
@@ -13,11 +15,11 @@ class DataStoreRepositoryImpl @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) : DataStoreRepository {
 
-    override suspend fun getDeviceToken(): Flow<String> {
+    override suspend fun getDeviceToken(): Flow<String>? {
         return dataStore.data
-            .map {
-                it[DEVICE_TOKEN] ?: ""
-            }
+            .map { preferences ->
+                preferences[DEVICE_TOKEN]?.let { flowOf(it) }
+            }.firstOrNull()
     }
 
     override suspend fun saveDeviceToken(deviceToken: String) {
--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/domain/repository/DataStoreRepository.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/domain/repository/DataStoreRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface DataStoreRepository {
     suspend fun getDeviceToken(): Flow<String>?
 
-    suspend fun saveDeviceToken(deviceToken: String = "")
+    suspend fun saveDeviceToken(deviceToken: String)
 
     suspend fun clearDataStore()
 }
