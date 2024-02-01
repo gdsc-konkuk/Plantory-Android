70e312dc2e590fb0f8389f9a6e64cfc4b5ffbc40
suyeun84 <kelsey6225@naver.com>
2024-02-01 21:54:32 +0900

    [del/#25] 주석삭제

--- app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/home/HomeViewModel.kt
+++ app/src/main/java/kr/ac/konkuk/gdsc/plantory/presentation/home/HomeViewModel.kt
@@ -9,33 +9,7 @@ class HomeViewModel : ViewModel() {
 
     private fun generateMockData(): List<Plant> {
         val mockDataList = mutableListOf<Plant>()
-
-//        val mockData1 = Plant(
-//            id = 1,
-//            dday = 2,
-//            nickname = "식물1",
-//            species = "선인장",
-//            description = "하이하이",
-//            "2023/12/31"
-//        )
-//
-//        val mockData2 = Plant(
-//            id = 2,
-//            dday = 2,
-//            nickname = "식물2",
-//            species = "선인장",
-//            description = "하이하이",
-//            "2023/12/31"
-//        )
-//
-//        val mockData3 = Plant(
-//            id = 3,
-//            dday = 2,
-//            nickname = "식물3",
-//            species = "선인장",
-//            description = "하이하이",
-//            "2023/12/31"
-//        )
+        
         val mockData1 = Plant(
             id = 1,
             imageUrl = null,
